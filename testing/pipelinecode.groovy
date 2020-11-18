dbe45d74a341b8d885ba745367a332f4d4a4ae5c
Gvsmaneesha <gvsmaneesha@gmail.com>
2020-11-18 11:27:00 +0530

    updated pipeline code

--- testing/pipelinecode.groovy
+++ testing/pipelinecode.groovy
@@ -1,1 +1,9 @@
-
+node('master')
+{
+        stage('build')
+        {
+            bat "python --version"
+            bat "git clone https://github.com/gvsmaneesha/datascience.git"
+            bat "dir"
+        }
+}
